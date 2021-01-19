e668d612767393d602dd9d64e1ecb5b46b318f66
Sivan Pe'er <psivi92@gmail.com>
2021-01-19 14:42:42 +0200

    milestone 1

--- test/StatLib.java
+++ test/StatLib.java
@@ -3,43 +3,70 @@ package test;
 
 public class StatLib {
 
-	
 
-	// simple average
-	public static float avg(float[] x){
-		return 0;
-	}
+    // simple average
+    public static float avg(float[] x) {
+        float avg = 0;
+        for (float v : x) {
+            avg += v;
+        }
+        avg /= x.length;
+        return avg;
+    }
 
-	// returns the variance of X and Y
-	public static float var(float[] x){
-		return 0;
-	}
+    // returns the variance of X and Y
+    public static float var(float[] x) {
+        float variance = 0;
+        for (int i = 0; i < x.length; i++) {
+            variance += Math.pow(x[i] - avg(x), 2);
+        }
+        variance /= x.length;
+        return variance;
+    }
 
-	// returns the covariance of X and Y
-	public static float cov(float[] x, float[] y){
+    // returns the covariance of X and Y
+    public static float cov(float[] x, float[] y) {
+        float covar = 0;
+        for (int i = 0; i < x.length; i++) {
+            covar += (x[i] - avg(x)) * (y[i] - avg(y));
+        }
+        covar /= x.length;
+        return covar;
+    }
 
-		return 0;
-	}
 
+    // returns the Pearson correlation coefficient of X and Y
+    public static float pearson(float[] x, float[] y) {
+        float covar = cov(x, y);
+        float vXvY = (float) Math.sqrt(var(x) * var(y));
+        return covar / vXvY;
+    }
 
-	// returns the Pearson correlation coefficient of X and Y
-	public static float pearson(float[] x, float[] y){
-		return 0;
-	}
+    // performs a linear regression and returns the line equation
+    public static Line linear_reg(Point[] points) {
+        float a, b;
+        Line Y;
+        float[] x = new float[points.length];
+        float[] y = new float[points.length];
+        for (int i = 0; i < points.length; i++) {
+            x[i] = points[i].x;
+            y[i] = points[i].y;
+        }
+        a = (cov(x, y) / var(x));
+        b = avg(y) - a * (avg(x));
+        Y = new Line(a, b);
+        return Y;
+    }
 
-	// performs a linear regression and returns the line equation
-	public static Line linear_reg(Point[] points){
-		return null;
-	}
+    // returns the deviation between point p and the line equation of the points
+    public static float dev(Point p, Point[] points) {
+        Line Y = linear_reg(points);
+        return dev(p, Y);
+    }
 
-	// returns the deviation between point p and the line equation of the points
-	public static float dev(Point p,Point[] points){
-		return 0;
-	}
-
-	// returns the deviation between point p and the line
-	public static float dev(Point p,Line l){
-		return 0;
-	}
-	
+    // returns the deviation between point p and the line
+    public static float dev(Point p, Line l) {
+        float res = Math.abs(p.y - l.f(p.x));
+        return res;
+    }
 }
